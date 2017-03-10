75398e4bf2aa2a3b23439d1c3bf9289cbdbd51dd
Onur bergal <onurbergal@gmail.com>
2017-03-10 12:13:42 +0200

    Update port-scanner.pl

--- port-scanner.pl
+++ port-scanner.pl
@@ -3,7 +3,6 @@ use strict;
 use IO::Socket::PortState qw(check_ports);
 
 my $hostfile = 'hosts.txt';
-
 my %port_hash = (
         tcp => {
             21      => {},
@@ -12,20 +11,16 @@ my %port_hash = (
             80      => {},
             }
         );
-
-my $timeout = 5;
-
-open HOSTS, '<', $hostfile or die "Cannot open $hostfile:$!\n";
-
-while (my $host = <HOSTS>) {
-    chomp($host);
-    my $host_hr = check_ports($host,$timeout,\%port_hash);
-    print "Host - $host\n";
-    for my $port (sort {$a <=> $b} keys %{$host_hr->{tcp}}) {
-        my $status = $host_hr->{tcp}{$port}{open} ? "open" : "closed";
-        print "$port - $status\n";
-
-}
-    }
-    print "\n";
+        my $timeout = 5;
+        open HOSTS, '<', $hostfile or die "Cannot open $hostfile:$!\n";
+        while (my $host = <HOSTS>) {
+                chomp($host);
+                my $host_hr = check_ports($host,$timeout,\%port_hash);
+                print "Host - $host\n";
+                for my $port (sort {$a <=> $b} keys %{$host_hr->{tcp}}) {
+                        my $status = $host_hr->{tcp}{$port}{open} ? "open" : "closed";
+                        print "$port - $status\n";
+                }
+        }
+        print "\n";
 }
